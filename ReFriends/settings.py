a07c08e5170d748ec5967da41f9e4c452e280664
i-marti <i-marti@example.com>
2026-01-03 09:39:26 -0600

    Update settings.py

--- ReFriends/settings.py
+++ ReFriends/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.timezone.TimezoneMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
 ROOT_URLCONF = "ReFriends.urls"

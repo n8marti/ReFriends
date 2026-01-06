f2bcb897fcb7aa7726801ae6671b3c17f5b98f11
i-marti <i-marti@example.com>
2026-01-06 13:22:22 -0600

    Added logging support to debug.log

--- ReFriends/settings.py
+++ ReFriends/settings.py
@@ -94,6 +94,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ReFriends.wsgi.application"
 
+# Logging: filepath is debug.log
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases

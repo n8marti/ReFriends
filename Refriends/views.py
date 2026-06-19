f2cd69300fb82af92598c9ead872059b985c636b
i-marti <i-marti@example.com>
2026-06-19 11:06:48 -0500

    Update views.py

--- Refriends/views.py
+++ Refriends/views.py
@@ -48,10 +48,7 @@ def set_timezone(request):
 
 def index(request):
     """The home page for ReFriends"""
-    if not request.user.is_authenticated:
-        return render(request, "Refriends/index.html")
-    else:
-        return redirect("Refriends:groups")
+    return render(request, "Refriends/index.html")
 
 
 @login_required
